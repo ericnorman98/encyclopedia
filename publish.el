0c32ff785f19acde86ed508f7ca496422a90ee1a
Eric Norman <eric.norman98@hotmail.se>
2021-04-08 07:48:44 +0200

    Use use-package n publish script

--- publish.el
+++ publish.el
@@ -1,25 +1,37 @@
 ;;; publish.el --- Publishing script -*- lexical-binding: t; -*-
 
-(add-to-list 'load-path ".")
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
 (require 'package)
+
+(setq package-user-dir (expand-file-name "~/encyclopedia/.pkgs"))
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                          ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-;;                           ("org" . "http://orgmode.org/elpa/")))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
 
-;; (package-refresh-contents)
+(use-package htmlize
+  :ensure t)
 
-(package-install 'htmlize)
-(package-install 'org-roam)
-(package-install 's)
+(use-package org-roam
+  :ensure t)
+
+(use-package s
+  :ensure t)
+
+(use-package jupyter
+  :ensure t)
 
 (require 'ox)
 (require 'ox-publish)
 (require 'ox-html)
-(require 'htmlize)
-(require 'org-roam)
-(require 's)
 (require 'encyclopedia)
 
 (setq make-backup-files nil)
@@ -34,12 +46,7 @@
 (setq encyclopedia-publish-sitemap-title "Eric's Encyclopedia")
 
 
-(setq org-confirm-babel-evaluate nil)
-(org-babel-do-load-languages
- 'org-babel-load-languages '((python . t)
-                             (jupyter . t)))
-
-(defun ericnorman/configure (publish-dir)
+(defun en/configure (publish-dir)
   (encyclopedia-publish-configure publish-dir)
 
   ;; this is important - otherwise org-roam--org-roam-file-p doesnt work.
@@ -47,17 +54,15 @@
   (setq org-roam-db-location "/home/eric/.emacs.d/.local/etc/org-roam.db"))
 
 
-(defun ericnorman/publish ()
-  (ericnorman/configure "/var/www/ericnorman")
+(defun en/publish ()
+  (en/configure "/var/www/ericnorman")
 
-  (rassq-delete-all 'html-mode auto-mode-alist)
-  (rassq-delete-all 'web-mode auto-mode-alist)
-  (fset 'web-mode (symbol-function 'fundamental-mode))
   (call-interactively 'org-publish-all))
+  ;; (encyclopedia-publish-files "/var/www/ericnorman"))
 
 
-(defun ericnorman/republish ()
-  (ericnorman/configure "/var/www/ericnorman")
+(defun en/republish ()
+  (en/configure "/var/www/ericnorman")
 
   (let ((current-prefix-arg 4))
     (rassq-delete-all 'web-mode auto-mode-alist)
